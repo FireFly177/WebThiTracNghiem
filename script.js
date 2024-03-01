487e219be77632c70be077db645a75fe34af4005
Duy Nguyen <huonghaiduynhim@gmail.com>
2024-03-01 14:38:53 +0700

    add login for normal user

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // set admin account
 localStorage.setItem("admin", "123")
+localStorage.setItem("duynguyen", "1772003")
 
 let loginToRegister = document.querySelector('.login-register'); //from-login-to-register button
 let registerToLogin = document.querySelector('.register-login'); // from register-to-login button
@@ -19,13 +20,29 @@ registerToLogin.addEventListener('click', () => {
 })
 
 // Login
+// Login
 let loginSubmitButton = document.querySelector('.loginSubmitButton'); // Login submit button
 let loginUsernameText = document.getElementById('loginUsernameText'); // Login username field
 let loginPasswordText = document.getElementById('loginPasswordText'); // Password username field
+
 loginSubmitButton.addEventListener('click', () => {
+    // Check if username is 'admin'
     if (loginUsernameText.value === 'admin') {
-        if (loginPasswordText.value === localStorage.getItem(loginUsernameText.value)) {
-            alert("Logged in as Admin")
+        if (loginPasswordText.value === localStorage.getItem('admin')) {
+            alert("Logged in as Admin. You have full privileges.");
+        } else {
+            alert("Wrong username or password.");
+        }
+    } else {
+        // Regular user login
+        let username = loginUsernameText.value;
+        let password = loginPasswordText.value;
+        
+        // Check if stored password matches entered password
+        if (localStorage.getItem(username) === password) {
+            alert(`Logged in as ${username}.`);
+        } else {
+            alert("Wrong username or password.");
         }
     }
-})
+});
