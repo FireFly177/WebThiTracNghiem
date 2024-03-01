34e39a7723946dce67547357b4141dc5afa58f63
Duy Nguyen <huonghaiduynhim@gmail.com>
2024-03-01 13:55:02 +0700

    clean up code files

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let registerToLogin = document.querySelector('.register-login');
 let registerForm = document.querySelector('.register');
 let loginForm = document.querySelector('.login');
 
+registerForm.classList.add('hidden')
+loginForm.classList.add('hidden')
+
 loginToRegister.addEventListener('click', () => {
     registerForm.classList.add('active');
     loginForm.classList.add('active');
