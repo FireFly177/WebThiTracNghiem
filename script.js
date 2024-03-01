7ee31c985e3f68e2a5aca183a91db889fc3d71d4
hao <haohoang30092003@gmail.com>
2024-03-01 18:55:52 +0700

    register update

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ registerToLogin.addEventListener('click', () => {
     registerForm.classList.remove('active');
     loginForm.classList.remove('active');
 })
+// Register
+let registerSubmitButton = document.querySelector(".registerSubmitButton");
+
+let registerUserName = document.getElementById("Register-username");
+let registerEmail = document.getElementById("Register-email");
+let registerPassword = document.getElementById("Register-password");
+let registerConfirm = document.getElementById("Register-confirm");
+// let registerUserNameText = registerUserName.value; // Using value property for input field
+registerSubmitButton.addEventListener('click', () => {
+    if (registerPassword.value != registerConfirm.value) {
+        alert("The confirmation password doesn't match!!!")
+    }
+    // let registerConfirm = document.getElementById("Register-confirm");
+
+    if (registerConfirm.value == null || registerConfirm.value.trim() === "") {
+        registerConfirm.style.background = "red"; // Change border to red
+    } else {
+        registerConfirm.style.border = ""; // Reset border to default
+    }
+
+});
+// || registerEmail == null || registerPassword == null || registerUserName== null
