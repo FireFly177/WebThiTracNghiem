ad9f38c3780f6190c80ea410fd7daeda011eb65d
Duy Nguyen <huonghaiduynhim@gmail.com>
2024-03-01 14:26:49 +0700

    add Admin login

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let loginToRegister = document.querySelector('.login-register');
-let registerToLogin = document.querySelector('.register-login');
-let registerForm = document.querySelector('.register');
-let loginForm = document.querySelector('.login');
+// set admin account
+localStorage.setItem("admin", "123")
 
-registerForm.classList.add('hidden')
-loginForm.classList.add('hidden')
+let loginToRegister = document.querySelector('.login-register'); //from-login-to-register button
+let registerToLogin = document.querySelector('.register-login'); // from register-to-login button
+let registerForm = document.querySelector('.register'); // Register form
+let loginForm = document.querySelector('.login'); // Login form
 
+
+// Change between login and register
 loginToRegister.addEventListener('click', () => {
     registerForm.classList.add('active');
     loginForm.classList.add('active');
@@ -14,4 +16,16 @@ loginToRegister.addEventListener('click', () => {
 registerToLogin.addEventListener('click', () => {
     registerForm.classList.remove('active');
     loginForm.classList.remove('active');
+})
+
+// Login
+let loginSubmitButton = document.querySelector('.loginSubmitButton'); // Login submit button
+let loginUsernameText = document.getElementById('loginUsernameText'); // Login username field
+let loginPasswordText = document.getElementById('loginPasswordText'); // Password username field
+loginSubmitButton.addEventListener('click', () => {
+    if (loginUsernameText.value === 'admin') {
+        if (loginPasswordText.value === localStorage.getItem(loginUsernameText.value)) {
+            alert("Logged in as Admin")
+        }
+    }
 })
